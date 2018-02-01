dadcd68eb5f039ee34ec123ea3746a1b08a3d1b7
onyxbronx <56aa34b07608cb272b1fe7332a0d54b0042cfc85>
2018-02-01 11:57:18 -0200

    Initial check-in of module MoNeg

---
+++ Droid/MainActivity.cs
@@ -0,0 +1,45 @@
+﻿using Android.App;
+using Android.Content;
+using Android.Content.PM;
+using Android.OS;
+using CarouselView.FormsPlugin.Android;
+using Com.OneSignal;
+using PerpetualEngine.Storage;
+using Xamarin.Forms;
+
+namespace MoNeg.Droid
+{
+    [Activity(Label = "Bradesco Monitoração", Icon = "@drawable/icone_app", Theme = "@style/MyTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            TabLayoutResource = Resource.Layout.Tabbar;
+            ToolbarResource = Resource.Layout.Toolbar;
+
+            base.OnCreate(bundle);
+
+            SimpleStorage.EditGroup = (string groupName) =>
+            {
+                return new DroidSimpleStorage(groupName, this);
+            };
+
+            global::Xamarin.Forms.Forms.Init(this, bundle);
+
+            CarouselViewRenderer.Init();
+
+            LoadApplication(new App());
+
+            OneSignal.Current.StartInit("d3a754b2-b601-4f1f-b016-493474fa99cb").EndInit();
+
+            Context context = this.ApplicationContext;
+			Settings.Default.Build = context.PackageManager.GetPackageInfo(context.PackageName, 0).VersionCode.ToString();
+            Settings.Default.Version = context.PackageManager.GetPackageInfo(context.PackageName, 0).VersionName;
+        }
+
+        protected override void OnActivityResult(int requestCode, Result resultCode, Intent data)
+        {
+            base.OnActivityResult(requestCode, resultCode, data);
+        }
+    }
+}
---
+++ iOS/AppDelegate.cs
@@ -0,0 +1,47 @@
+﻿using System.Net;
+using System.Threading;
+using CarouselView.FormsPlugin.iOS;
+using Com.OneSignal;
+using Foundation;
+using KeyboardOverlap.Forms.Plugin.iOSUnified;
+using Syncfusion.ListView.XForms.iOS;
+using Syncfusion.SfBusyIndicator.XForms.iOS;
+using Syncfusion.SfChart.XForms.iOS.Renderers;
+using Syncfusion.SfDataGrid.XForms.iOS;
+using Syncfusion.SfNavigationDrawer.XForms.iOS;
+using Syncfusion.SfPullToRefresh.XForms.iOS;
+using UIKit;
+
+namespace MoNeg.iOS
+{
+    [Register("AppDelegate")]
+    public partial class AppDelegate : global::Xamarin.Forms.Platform.iOS.FormsApplicationDelegate
+    {
+        public override bool FinishedLaunching(UIApplication app, NSDictionary options)
+        {
+			ServicePointManager.ServerCertificateValidationCallback +=
+	            (sender, cert, chain, sslPolicyErrors) => true;
+
+			global::Xamarin.Forms.Forms.Init();
+
+            CarouselViewRenderer.Init();
+
+            KeyboardOverlapRenderer.Init();
+            SfDataGridRenderer.Init();
+            SfListViewRenderer.Init();
+            SfPullToRefreshRenderer.Init();
+            new SfChartRenderer();
+            new SfBusyIndicatorRenderer();
+
+            LoadApplication(new App());
+            new SfNavigationDrawerRenderer();
+
+			OneSignal.Current.StartInit("d3a754b2-b601-4f1f-b016-493474fa99cb").EndInit();
+
+            Settings.Default.Build = NSBundle.MainBundle.InfoDictionary[new NSString("CFBundleVersion")].ToString();
+            Settings.Default.Version = NSBundle.MainBundle.InfoDictionary[new NSString("CFBundleShortVersionString")].ToString();
+
+            return base.FinishedLaunching(app, options);
+        }
+    }
+}
